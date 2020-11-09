4f1aff27a347b43131f23aefdf40e74b3ba2a412
Löffler Levente <levente.loffler99@gmail.com>
2020-11-09 20:59:43 +0100

    Fixed arrow bug by decreasing physics period.

---
+++ Assets/Scripts/MainScene/Utility/PhysicsManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+public class PhysicsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Time.fixedDeltaTime = 0.005f;
+    }
+}
