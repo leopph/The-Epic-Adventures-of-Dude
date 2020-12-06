892150b351823ccdb45c046586b038dc8a540467
Löffler Levente <levente.loffler99@gmail.com>
2020-12-06 11:05:41 +0100

    More fps counter tuning.

---
+++ Assets/Scripts/MainScene/HUD/FPSCounter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+
+
+
+public class FPSCounter : MonoBehaviour
+{
+    private string m_KeyName = "FPSCounter";
+    private System.Text.StringBuilder m_StringBuilder = new System.Text.StringBuilder();
+
+    private TextMeshProUGUI m_Text;
+    private int m_FPS;
+    private double m_FrameTime;
+
+
+
+
+    private void Awake()
+    {
+        m_Text = GetComponent<TextMeshProUGUI>();
+        if (!PlayerPrefs.HasKey(m_KeyName))
+        {
+            PlayerPrefs.SetInt(m_KeyName, 0);
+            m_Text.text = "";
+        }
+    }
+
+
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            m_FPS = Mathf.RoundToInt(1 / Time.unscaledDeltaTime);
+            m_FrameTime = System.Math.Round(1000 * Time.unscaledDeltaTime, 2);
+            yield return new WaitForSecondsRealtime(0.5f);
+        }    
+    }
+
+
+
+    private void OnGUI()
+    {
+        if (PlayerPrefs.GetInt(m_KeyName) == 1)
+            m_Text.text = m_StringBuilder.Clear().AppendLine(SystemInfo.graphicsDeviceType.ToString()).Append(m_FPS).AppendLine(" FPS").Append(m_FrameTime).Append(" ms").ToString();
+        else
+            m_Text.text = "";
+    }
+}
