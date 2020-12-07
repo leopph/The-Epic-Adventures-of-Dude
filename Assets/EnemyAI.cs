731d58a4da262541459a6c2f47de2c81f7390950
Löffler Levente <levente.loffler99@gmail.com>
2020-12-07 21:42:11 +0100

    Working basic pathfinding and ai.

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+
+[RequireComponent(typeof(EnemyPathfinding))]
+
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum State { Idle, Chasing, Combat, Retreating }
+
+    private State m_State = State.Idle;
+    private Vector3 m_StartingPosition;
+    private Player m_Player;
+    private EnemyPathfinding m_Pathfinding;
+
+    public float m_IdleWalkDistance;
+    public float m_ChaseRange;
+    public float m_AttackRange;
+    public float m_MaxDistanceFromStart;
+
+
+    private void Awake()
+    {
+        Assert.IsTrue(m_IdleWalkDistance <= m_MaxDistanceFromStart, "MAX WALK DISTANCE IS BIGGER THAN THE ALLOWED MAX DISTANCE FROM SPAWN");
+
+        m_StartingPosition = transform.position;
+        m_Pathfinding = GetComponent<EnemyPathfinding>();
+    }
+
+    private void Start() { m_Player = GameObject.Find("Dood").GetComponent<Player>(); }
+
+
+    void Update()
+    {
+        if (m_State != State.Retreating && Vector3.Distance(transform.position, m_StartingPosition) > m_MaxDistanceFromStart)
+            ChangeState(State.Retreating);
+
+        switch(m_State)
+        {
+            case State.Idle:
+                if (Vector3.Distance(transform.position, m_Player.transform.position) <= m_ChaseRange)
+                    ChangeState(State.Chasing);
+
+                else if (!m_Pathfinding.moving)
+                {
+                    float distance = transform.position.x - m_StartingPosition.x;
+                    float newDistance = Random.value * m_IdleWalkDistance * (distance > 0 ? -1 : 1);
+
+                    m_Pathfinding.MoveTo(m_StartingPosition + new Vector3(newDistance, 0));
+                }
+                break;
+
+            case State.Chasing:
+                if (Vector3.Distance(m_Player.transform.position, transform.position) < m_AttackRange)
+                    ChangeState(State.Combat);
+
+                else if (!m_Pathfinding.moving)
+                    m_Pathfinding.Track(m_Player.transform);
+                break;
+
+            case State.Combat:
+                break;
+
+            case State.Retreating:
+                if (!m_Pathfinding.moving)
+                    if (Mathf.Abs(m_StartingPosition.x - transform.position.x) <= m_IdleWalkDistance)
+                        ChangeState(State.Idle);
+                    else
+                    {
+                        Vector3 target = m_StartingPosition + new Vector3(transform.position.x > m_StartingPosition.x ? m_IdleWalkDistance : -m_IdleWalkDistance, 0);
+                        m_Pathfinding.MoveTo(target);
+                    }
+                break;
+        }
+    }
+
+
+    private void ChangeState(State newState)
+    {
+        m_Pathfinding.StopMoving();
+        m_State = newState;
+        Debug.Log(m_State);
+    }
+}
---
+++ Assets/EnemyPathfinding.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    public float m_Speed = 2f;
+    public bool m_VerticalMovement = false;
+
+    private Path m_Path;
+    private Seeker m_Seeker;
+
+    private float m_NextWaypointDistance = 0.5f;
+    private int m_CurrentWaypoint = 0;
+
+    private Vector2 m_Target;
+    private Transform m_TrackedTarget;
+    private bool m_Moving = false;
+    public bool moving => m_Moving;
+
+
+
+
+    private void Awake() { m_Seeker = GetComponent<Seeker>(); }
+
+
+
+    private void Update()
+    {
+        if (!m_Moving)
+            return;
+
+        if (m_Path == null)
+            return;
+
+        if (m_CurrentWaypoint >= m_Path.vectorPath.Count)
+        {
+            if (Mathf.Approximately(transform.position.x, m_Target.x))
+                StopMoving();
+            else
+            {
+                if (Mathf.Abs(transform.position.x - m_Target.x) < m_Speed * Time.deltaTime)
+                    transform.position = new Vector3(m_Target.x, transform.position.y, transform.position.z);
+                else
+                    transform.Translate(new Vector2(m_Target.x > transform.position.x ? 1 : -1, 0) * m_Speed * Time.deltaTime);
+            }
+            return;
+        }
+
+        transform.Translate(new Vector2(m_Path.vectorPath[m_CurrentWaypoint].x - transform.position.x, 0).normalized * m_Speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, m_Path.vectorPath[m_CurrentWaypoint]) < m_NextWaypointDistance)
+            m_CurrentWaypoint++;
+    }
+
+
+
+    public void MoveTo(Vector2 position)
+    {
+        StopMoving();
+
+        m_Target = m_VerticalMovement ? position : new Vector2(position.x, transform.position.y);
+        m_Moving = true;
+        m_CurrentWaypoint = 0;
+        InvokeRepeating("PathUpdate", 0f, 0.5f);
+    }
+
+
+    public void Track(Transform transform)
+    {
+        m_TrackedTarget = transform;
+        MoveTo(m_Target);
+        InvokeRepeating("TargetUpdate", 0f, 0.25f);
+    }
+
+
+
+    public void StopMoving()
+    {
+        CancelInvoke("PathUpdate");
+        CancelInvoke("TargetUpdate");
+        m_Moving = false;
+    }
+
+
+
+    public void TargetUpdate() { m_Target = m_TrackedTarget.position; Debug.Log(m_Target); }
+
+
+
+    private void PathUpdate()
+    {
+        if (m_Seeker.IsDone())
+        {
+            if (!m_VerticalMovement)
+                m_Target.y = transform.position.y;
+
+            m_Seeker.StartPath(transform.position, m_Target, OnPathComplete);
+        }
+    }
+
+
+
+    private void OnPathComplete(Path path)
+    {
+        if (!path.error)
+        {
+            m_Path = path;
+            m_CurrentWaypoint = 0; 
+        }
+    }
+}
