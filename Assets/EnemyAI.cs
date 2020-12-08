2886a17ec6063c733d77e5d7f4c57287de25f8bc
Löffler Levente <levente.loffler99@gmail.com>
2020-12-08 09:23:37 +0100

    Working basic attack handling.

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -13,6 +13,8 @@ public class EnemyAI : MonoBehaviour
     private Vector3 m_StartingPosition;
     private Player m_Player;
     private EnemyPathfinding m_Pathfinding;
+    private EnemyAttack m_EnemyAttack;
+    private Animator m_Animator;
 
     public float m_IdleWalkDistance;
     public float m_ChaseRange;
@@ -26,9 +28,14 @@ public class EnemyAI : MonoBehaviour
 
         m_StartingPosition = transform.position;
         m_Pathfinding = GetComponent<EnemyPathfinding>();
+        m_EnemyAttack = GetComponent<EnemyAttack>();
     }
 
-    private void Start() { m_Player = GameObject.Find("Dood").GetComponent<Player>(); }
+    private void Start()
+    {
+        m_Animator = GetComponentInChildren<Animator>();
+        m_Player = GameObject.Find("Dood").GetComponent<Player>();
+    }
 
 
     void Update()
@@ -36,44 +43,75 @@ public class EnemyAI : MonoBehaviour
         if (m_State != State.Retreating && Mathf.Abs(transform.position.x - m_StartingPosition.x) > m_MaxDistanceFromStart)
             ChangeState(State.Retreating);
 
-        switch(m_State)
+        switch (m_State)
         {
             case State.Idle:
-                if (Mathf.Abs(transform.position.x - m_Player.transform.position.x) <= m_ChaseRange && Mathf.Abs(m_StartingPosition.x - m_Player.transform.position.x) <= m_StartingPosition.x + m_MaxDistanceFromStart)
-                    ChangeState(State.Chasing);
-
-                else if (!m_Pathfinding.moving)
                 {
-                    float distance = transform.position.x - m_StartingPosition.x;
-                    float newDistance = Random.value * m_IdleWalkDistance * (distance > 0 ? -1 : 1);
+                    if (Mathf.Abs(transform.position.x - m_Player.transform.position.x) <= m_ChaseRange && Mathf.Abs(m_StartingPosition.x - m_Player.transform.position.x) <= m_StartingPosition.x + m_MaxDistanceFromStart)
+                    {
+                        ChangeState(State.Chasing);
+                    }
+                    else if (!m_Pathfinding.moving)
+                    {
+                        float distance = transform.position.x - m_StartingPosition.x;
+                        float newDistance = Random.value * m_IdleWalkDistance * (distance > 0 ? -1 : 1);
 
-                    m_Pathfinding.MoveTo(m_StartingPosition + new Vector3(newDistance, 0));
+                        m_Pathfinding.MoveTo(m_StartingPosition + new Vector3(newDistance, 0));
+                    }
+                    break;
                 }
-                break;
+
 
             case State.Chasing:
-                if (Mathf.Abs(m_Player.transform.position.x - transform.position.x) <= m_AttackRange)
-                    ChangeState(State.Combat);
+                {
+                    if (Mathf.Abs(m_Player.transform.position.x - transform.position.x) <= m_AttackRange)
+                    {
+                        ChangeState(State.Combat);
+                    }
+                    else if (!m_Pathfinding.moving)
+                    {
+                        m_Pathfinding.Track(m_Player.transform);
+                    }
+                    break;
+                }
 
-                else if (!m_Pathfinding.moving)
-                    m_Pathfinding.Track(m_Player.transform);
-                break;
 
             case State.Combat:
-                break;
-
-            case State.Retreating:
-                if (!m_Pathfinding.moving)
-                    if (Mathf.Abs(m_StartingPosition.x - transform.position.x) <= m_IdleWalkDistance)
-                        ChangeState(State.Idle);
+                {
+                    if (Mathf.Abs(m_Player.transform.position.x - transform.position.x) > m_AttackRange && !m_EnemyAttack.attacking)
+                    {
+                        ChangeState(State.Chasing);
+                    }
                     else
                     {
-                        Vector3 target = m_StartingPosition + new Vector3(transform.position.x > m_StartingPosition.x ? m_IdleWalkDistance : -m_IdleWalkDistance, 0);
-                        m_Pathfinding.MoveTo(target);
+                        if (!m_EnemyAttack.attacking)
+                            m_EnemyAttack.Attack();
                     }
-                break;
+                    break;
+                }
+
+
+            case State.Retreating:
+                {
+                    if (!m_Pathfinding.moving)
+                    {
+                        if (Mathf.Abs(m_StartingPosition.x - transform.position.x) <= m_IdleWalkDistance)
+                        {
+                            ChangeState(State.Idle);
+                        }
+                        else
+                        {
+                            Vector3 target = m_StartingPosition + new Vector3(transform.position.x > m_StartingPosition.x ? m_IdleWalkDistance : -m_IdleWalkDistance, 0);
+                            m_Pathfinding.MoveTo(target);
+                        }
+                    }
+                    break;
+                }
         }
+
+        m_Animator.SetBool("Moving", m_Pathfinding.moving);
     }
+
 
 
     private void ChangeState(State newState)
---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+
+
+
+public class EnemyAttack : MonoBehaviour
+{
+    private Animator m_Animator;
+    private bool m_Attacking = false;
+    public bool attacking => m_Attacking;
+
+    private float m_DeltaTime = 0f;
+    public float m_AttackSpeed = 1f;
+
+
+
+
+    private void Start()
+    {
+        m_Animator = GetComponentInChildren<Animator>();
+    }
+
+
+    private void Update()
+    {
+        if (attacking)
+        {
+            m_DeltaTime += Time.deltaTime;
+
+            if (m_DeltaTime > 1 / m_AttackSpeed)
+                m_Attacking = false;
+        }
+    }
+
+
+    public void Attack()
+    {
+        m_Animator.SetFloat("AttackSpeed", m_AttackSpeed);
+        m_Animator.SetTrigger("Attacking");
+        m_Attacking = true;
+        m_DeltaTime = 0f;
+    }
+}
